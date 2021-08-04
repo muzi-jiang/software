0593aa95f221a49b40c129f92c3ff81251634e26
libin <libin9@dgg.net>
2021-08-04 16:31:10 +0800

    初始化项目

---
+++ website/website-api/src/main/java/com/muzi/website/StartApplication.java
@@ -0,0 +1,8 @@
+package com.muzi.website;
+
+/**
+ * @Author li.bin
+ * @Date 2021年08月04日 16:30
+ */
+public class StartApplication {
+}
---
+++ website/website-service/src/main/java/A.java
@@ -0,0 +1,6 @@
+/**
+ * @Author li.bin
+ * @Date 2021年08月04日 16:30
+ */
+public class A {
+}
