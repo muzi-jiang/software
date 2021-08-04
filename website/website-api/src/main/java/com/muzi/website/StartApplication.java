8594ec2a6682818572e181068cd03af86e056565
libin <libin9@dgg.net>
2021-08-04 16:48:56 +0800

    初始化项目

--- website/website-api/src/main/java/com/muzi/website/StartApplication.java
+++ website/website-api/src/main/java/com/muzi/website/StartApplication.java
@@ -1,8 +1,17 @@
 package com.muzi.website;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author li.bin
  * @Date 2021年08月04日 16:30
  */
+
+@SpringBootApplication
 public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
 }
--- website/website-service/src/main/java/A.java
+++
@@ -1,6 +0,0 @@
-/**
- * @Author li.bin
- * @Date 2021年08月04日 16:30
- */
-public class A {
-}
